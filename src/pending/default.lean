9f45723a14c927c195f6e9c1063bcc52d5369956
kckennylau <kc_kennylau@yahoo.com.hk>
2019-02-01 13:10:53 +0000

    refactor(*): refactor associates

--- src/pending/default.lean
+++ src/pending/default.lean
@@ -1,1 +1,53 @@
 /- Temporary space for definitions pending merges to the lean repository -/
+
+import algebra.gcd_domain
+
+theorem irreducible_iff_nat_prime : ∀(a : ℕ), irreducible a ↔ nat.prime a
+| 0 := by simp [nat.not_prime_zero]
+| 1 := by simp [nat.prime, one_lt_two]
+| (n + 2) :=
+  have h₁ : ¬n + 2 = 1, from dec_trivial,
+  begin
+    simp [h₁, nat.prime, irreducible, (≥), nat.le_add_left 2 n, (∣)],
+    refine forall_congr (assume a, forall_congr $ assume b, forall_congr $ assume hab, _),
+    by_cases a = 1; simp [h],
+    split,
+    { assume hb, simpa [hb] using hab.symm },
+    { assume ha, subst ha,
+      have : n + 2 > 0, from dec_trivial,
+      refine nat.eq_of_mul_eq_mul_left this _,
+      rw [← hab, mul_one] }
+  end
+
+lemma nat.prime_iff_prime {p : ℕ} : p.prime ↔ _root_.prime (p : ℕ) :=
+⟨λ hp, ⟨nat.pos_iff_ne_zero.1 hp.pos, mt is_unit_iff_dvd_one.1 hp.not_dvd_one,
+    λ a b, hp.dvd_mul.1⟩,
+  λ hp, ⟨nat.one_lt_iff_ne_zero_and_ne_one.2 ⟨hp.1, λ h1, hp.2.1 $ h1.symm ▸ is_unit_one⟩,
+    λ a h, let ⟨b, hab⟩ := h in
+      (hp.2.2 a b (hab ▸ dvd_refl _)).elim
+        (λ ha, or.inr (nat.dvd_antisymm h ha))
+        (λ hb, or.inl (have hpb : p = b, from nat.dvd_antisymm hb
+            (hab.symm ▸ dvd_mul_left _ _),
+          (nat.mul_left_inj (show 0 < p, from
+              nat.pos_of_ne_zero hp.1)).1 $
+            by rw [hpb, mul_comm, ← hab, hpb, mul_one]))⟩⟩
+
+lemma nat.prime_iff_prime_int {p : ℕ} : p.prime ↔ _root_.prime (p : ℤ) :=
+⟨λ hp, ⟨int.coe_nat_ne_zero_iff_pos.2 hp.pos, mt is_unit_int.1 hp.ne_one,
+  λ a b h, by rw [← int.dvd_nat_abs, int.coe_nat_dvd, int.nat_abs_mul, hp.dvd_mul] at h;
+    rwa [← int.dvd_nat_abs, int.coe_nat_dvd, ← int.dvd_nat_abs, int.coe_nat_dvd]⟩,
+  λ hp, nat.prime_iff_prime.2 ⟨int.coe_nat_ne_zero.1 hp.1,
+      mt is_unit_nat.1 $ λ h, by simpa [h, not_prime_one] using hp,
+    λ a b, by simpa only [int.coe_nat_dvd, (int.coe_nat_mul _ _).symm] using hp.2.2 a b⟩⟩
+
+local attribute [instance] associated.setoid
+
+def associates_int_equiv_nat : (associates ℤ) ≃ ℕ :=
+begin
+  refine ⟨λz, z.out.nat_abs, λn, associates.mk n, _, _⟩,
+  { refine (assume a, quotient.induction_on a $ assume a,
+      associates.mk_eq_mk_iff_associated.2 $ associated.symm $ ⟨norm_unit a, _⟩),
+    simp [associates.out_mk, associates.quotient_mk_eq_mk, associated,
+      int.coe_nat_abs_eq_mul_norm_unit.symm] },
+  { assume n, simp [associates.out_mk, int.coe_nat_abs_eq_mul_norm_unit.symm] }
+end
